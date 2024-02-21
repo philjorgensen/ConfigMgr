13ae8a5c2c46b6fe52672a676a5c51647c063f45
Philip Jorgensen <pjorgensen@lenovo.com>
2024-02-21 14:19:17 -0500

    Update Detect-CommercialVantage.ps1
    Script redesign

--- Applications/Detect-CommercialVantage.ps1
+++ Applications/Detect-CommercialVantage.ps1
@@ -1,27 +1,55 @@
-$ErrorActionPreference = "SilentlyContinue"
+# Change this variable to the version you're deploying
+$DeployedVantageVersion = "10.2310.28.0"
 
-If (Get-AppxPackage -Name E046963F.LenovoSettingsforEnterprise -all) {
+$ErrorActionPreference = "Stop"
 
-    If (Get-Service -Name ImControllerService) {
+try
+{
 
-        If (Get-Service -Name LenovoVantageService) {
-
-            # Check for older of version of Vantage Service that causes UAC prompt. This is due to an expired certificate.  
-            $minVersion = "3.8.23.0"
-            $path = ${env:ProgramFiles(x86)} + "\Lenovo\VantageService\*\LenovoVantageService.exe"
-            $version = (Get-ChildItem -Path $path).VersionInfo.FileVersion
-            
-            if ([version]$version -ge [version]$minVersion) {
-                
-                Write-Host "All Commercial Vantage Services and App are installed..."
-            }
-
-            else {
-            
-            }
-        
-        }
+    If (Get-Service -Name ImControllerService)
+    {
     
     }
+        
+    If (Get-Service -Name LenovoVantageService)
+    {
+        # Check for older of version of Vantage Service that causes UAC prompt. This is due to an expired certificate.  
+        $minVersion = "3.8.23.0"
+        $path = ${env:ProgramFiles(x86)} + "\Lenovo\VantageService\*\LenovoVantageService.exe"
+        $version = (Get-ChildItem -Path $path).VersionInfo.FileVersion
+            
+        if ([version]$version -le [version]$minVersion)
+        {
+            
+            Write-Output "Vantage Service outdated."
+        }
+    }
+        
+    # Assume no version is installed
+    $InstalledVersion = $false
+    
+    # For specific Appx version
+    $InstalledVantageVersion = (Get-AppxPackage -Name E046963F.LenovoSettingsforEnterprise -AllUsers).Version
 
+    If ([version]$InstalledVantageVersion -ge [version]$DeployedVantageVersion)
+    {
+        $InstalledVersion = $true
+        
+        # For package name only    
+        # If (Get-AppxPackage -Name E046963F.LenovoSettingsforEnterprise -AllUsers) {
+
+    }
+
+    if ($InstalledVersion)
+    {
+        Write-Host "All Vantage Services and Appx Present"
+    }
+    else
+    {
+        # Write-Output "Commercial Vantage is outdated."
+    }
+}
+catch
+{
+    
 }
